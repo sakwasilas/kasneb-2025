b129ae7645c88dce8f360b7ea7d879a03fe54d91
sakwasilas <sakwahsilas@gmail.com>
2025-07-09 10:13:18 +0300

    migrated the databse

--- connection.py
+++ connection.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "mysql+mysqldb://root:2480@localhost/stc_db"
+DATABASE_URL = "sqlite:///kasneb.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
